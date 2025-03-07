f4b6e15f9bcf81112f5cf01704580fae609fc726
Samba231 <125780014+Samba231@users.noreply.github.com>
2025-03-07 13:36:56 -0500

    Update identity.ts

--- configuration/identity.ts
+++ configuration/identity.ts
@@ -2,5 +2,5 @@ export const OWNER_NAME: string = `Samba`;
 export const OWNER_DESCRIPTION: string = `Help with CS topics `;
 
 export const AI_NAME: string = `Lays`;
-export const AI_TONE: string = `[AI TONE]`;
+export const AI_TONE: string = `Educational,beginner-friendly, and technical`;
 export const AI_ROLE: string = `AI Tutor`;
