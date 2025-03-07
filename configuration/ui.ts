fca65c0d3a70f3df5d34541663aa4436bee406f1
Samba231 <125780014+Samba231@users.noreply.github.com>
2025-03-07 06:13:43 -0500

    Update ui.ts

--- configuration/ui.ts
+++ configuration/ui.ts
@@ -1,10 +1,10 @@
 import { AI_NAME, OWNER_NAME } from "@/configuration/identity";
 
-export const CHAT_HEADER: string = `Here to Assist `;
+export const CHAT_HEADER: string = `Here to Assist! `;
 export const MESSAGE_PLACEHOLDER: string = `[MESSAGE PLACEHOLDER]`;
-export const FOOTER_MESSAGE: string = `AI by Samba, PS: AI can make Mistakes!! `;
+export const FOOTER_MESSAGE: string = `AI can make Mistakes!! `;
 export const CLEAR_BUTTON_TEXT: string = `Clear Chat`;
-export const PAGE_TITLE: string = `[PAGE TITLE]`;
+export const PAGE_TITLE: string = `Lays's Chat`;
 export const PAGE_DESCRIPTION: string = `Chat with ${AI_NAME}, ${OWNER_NAME}'s AI assistant.`;
 
 export const EMPTY_CITATION_MESSAGE: string = "Unspecified source";
